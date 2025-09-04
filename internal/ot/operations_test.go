5548068e13a8bfee6f0fb460709d6f3e68a584c6
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2025-09-04 00:01:36 -0500

    Add support for rebasing operations

--- internal/ot/operations_test.go
+++ internal/ot/operations_test.go
@@ -64,3 +64,125 @@ func TestDelete(t *testing.T) {
 		}
 	}
 }
+
+func TestOperationCommutativity(t *testing.T) {
+	var cases = []struct {
+		start    []byte
+		opA, opB ot.Operation
+	}{
+		{
+			start: []byte(""),
+			opA:   ot.Insertion{Pos: 0, Text: []byte("hello")},
+			opB:   ot.Insertion{Pos: 0, Text: []byte("world")},
+		},
+		{
+			start: []byte("hello"),
+			opA:   ot.Insertion{Pos: 5, Text: []byte(" world")},
+			opB:   ot.Deletion{Pos: 0, Len: 5},
+		},
+	}
+
+	for _, c := range cases {
+		a, b := c.start, c.start
+		opA, opB := c.opA, c.opB
+
+		a = opA.Apply(a)
+		a = opB.Rebase(opA).Apply(a)
+		b = opB.Apply(b)
+		b = opA.Rebase(opB).Apply(b)
+
+		if string(a) != string(b) {
+			t.Errorf("operations depend on order (%q != %q): %+v, %+v", a, b, opA, opB)
+		}
+	}
+}
+
+func valid(start string, op ot.Operation) bool {
+	switch op := op.(type) {
+	case ot.Insertion:
+		return int(op.Pos) <= len([]byte(start))
+	case ot.Deletion:
+		return int(op.Pos)+int(op.Len) <= len([]byte(start))
+	}
+	panic("unhandled operation type")
+}
+
+func FuzzInsertInsertCommutativity(f *testing.F) {
+	f.Add("0000", uint(0), uint(3), []byte("hello"), []byte("world"))
+	f.Add("0000", uint(2), uint(2), []byte("hello"), []byte("world"))
+	f.Add("0000", uint(0), uint(0), []byte("hello"), []byte("hello"))
+	f.Fuzz(func(t *testing.T, start string, posA, posB uint, textA, textB []byte) {
+		opA := ot.Insertion{Pos: posA, Text: textA}
+		opB := ot.Insertion{Pos: posB, Text: textB}
+
+		if !valid(start, opA) || !valid(start, opB) {
+			t.Skip()
+		}
+
+		a, b := []byte(start), []byte(start)
+
+		a = opA.Apply(a)
+		a = opB.Rebase(opA).Apply(a)
+		b = opB.Apply(b)
+		b = opA.Rebase(opB).Apply(b)
+
+		if string(a) != string(b) {
+			t.Errorf("operations depend on order (%q != %q): %+v, %+v", a, b, opA, opB)
+		}
+	})
+}
+
+func FuzzInsertDeleteCommutativity(f *testing.F) {
+	f.Add("0000", uint(0), uint(3), []byte("hello"), uint(1))
+	f.Add("0000", uint(1), uint(1), []byte("hello"), uint(3))
+	f.Add("0000", uint(1), uint(0), []byte("hello"), uint(3))
+	f.Fuzz(func(t *testing.T, start string, posA, posB uint, textA []byte, lenB uint) {
+		opA := ot.Insertion{Pos: posA, Text: textA}
+		opB := ot.Deletion{Pos: posB, Len: lenB}
+
+		if !valid(start, opA) || !valid(start, opB) {
+			t.Skip()
+		}
+
+		a, b := []byte(start), []byte(start)
+
+		a = opA.Apply(a)
+		a = opB.Rebase(opA).Apply(a)
+		b = opB.Apply(b)
+		b = opA.Rebase(opB).Apply(b)
+
+		if string(a) != string(b) {
+			t.Errorf("operations depend on order (%q != %q): %+v, %+v", a, b, opA, opB)
+		}
+	})
+}
+
+func FuzzDeleteDeleteCommutativity(f *testing.F) {
+	f.Add("0000", uint(0), uint(3), uint(2), uint(1))
+	f.Add("0000", uint(3), uint(0), uint(1), uint(2))
+	f.Add("0000", uint(0), uint(2), uint(3), uint(2))
+	f.Add("0000", uint(1), uint(1), uint(2), uint(1))
+	f.Add("0000", uint(1), uint(2), uint(3), uint(1))
+	f.Add("0000", uint(2), uint(0), uint(1), uint(4))
+	f.Add("0000", uint(0), uint(0), uint(1), uint(1))
+	f.Add("", uint(0), uint(0), uint(0), uint(0))
+	f.Fuzz(func(t *testing.T, start string, posA, posB uint, lenA, lenB uint) {
+		opA := ot.Deletion{Pos: posA, Len: lenA}
+		opB := ot.Deletion{Pos: posB, Len: lenB}
+
+		if !valid(start, opA) || !valid(start, opB) {
+			t.Skip()
+		}
+
+		a, b := []byte(start), []byte(start)
+
+		a = opA.Apply(a)
+		a = opB.Rebase(opA).Apply(a)
+		b = opB.Apply(b)
+		b = opA.Rebase(opB).Apply(b)
+
+		if string(a) != string(b) {
+			t.Errorf("operations depend on order (%q != %q): %q, %+v, %+v", a, b, start, opA, opB)
+		}
+	})
+}

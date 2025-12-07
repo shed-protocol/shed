60fab1471e094589b8aceb4debbe12e74092c348
Evan <evanne.scherrer@gmail.com>
2025-12-06 18:32:53 -0600

    set up server executable

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"log"
+	"os"
+
+	"github.com/shed-protocol/shed/internal/server"
 )
 
 func main() {
-	fmt.Println("server")
+	LISTEN_PORT := os.Args[1]
+
+	fmt.Println("Starting server...")
+
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%s", LISTEN_PORT))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	s := new(server.Server)
+	s.Init()
+	s.Start()
+	fmt.Sprintln("Listening on port %s", LISTEN_PORT)
 }

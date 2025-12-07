24c30c8d1b242f5968a240be534c6830167fe31e
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2025-12-06 19:24:53 -0600

    Add client executable (#13)

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"net"
+	"os"
+
+	"github.com/shed-protocol/shed/internal/client"
+)
+
+var c client.Client
 
 func main() {
-	fmt.Println("client")
+	c.Attach(stdio{})
+	server, err := net.Dial("tcp", os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	c.Connect(server)
+	for {
+	}
+}
+
+type stdio struct{}
+
+func (s stdio) Read(p []byte) (n int, err error) {
+	return os.Stdin.Read(p)
+}
+
+func (s stdio) Write(p []byte) (n int, err error) {
+	return os.Stdout.Write(p)
 }

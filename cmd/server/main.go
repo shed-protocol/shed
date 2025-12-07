057dd928b339175709082ed3815a935cc1bed5a1
Jovan-04 <112601020+Jovan-04@users.noreply.github.com>
2025-12-06 19:25:11 -0600

    Add server executable (#12)
    ---------
    
    Co-authored-by: Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/shed-protocol/shed/internal/server"
 )
 
 func main() {
-	fmt.Println("server")
+	LISTEN_PORT := os.Args[1]
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%s", LISTEN_PORT))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	var s server.Server
+	s.Init()
+	go s.Start()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			continue
+		}
+
+		s.Accept(conn)
+	}
 }
